db1ccb43271f91333cd456b3afad9cbf1389013a
sedrubal <sebastian.endres@online.de>
2015-11-07 23:12:34 +0100

    Use functions instead of if else cascade

--- manage-docker.sh
+++ manage-docker.sh
@@ -1,44 +1,60 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 IMAGE="app-server"
 CONTAINER="${IMAGE}"
-CONF_DIR="./conf/"
+CONF_DIR="${PWD}/conf/"
+VOL_LOC="${CONF_DIR}"
+VOL_DOC="/home/fablab/app-server/src/dist/"
 SHELL="bash"
 
 if [ $(whoami) != "root" ]; then echo "[i] this script has to be executed as root!" && exit 1; fi
 
-if [ "$1" == "port" ]; then
+function port() {
     docker port "${CONTAINER}"
-elif [ "$1" == "build" ]; then
+}
+function build() {
     docker build -t "${IMAGE}" .
-elif [ "$1" == "up" ]; then
+}
+function run() {
+    docker run -d -p 80 -p 8081 --volume="${VOL_LOC}:${VOL_DOC}" \
+        --name="${CONTAINER}" "${IMAGE}"
+}
+function up() {
     if [ ! -e "${CONF_DIR}/config.yml" ] && [ ! -e "${CONF_DIR}/minimumVersion.yml" ]; then
         echo "[x] ${CONF_DIR}/config.yml or ${CONF_DIR}/minimumVersion.yml is missing"
         exit 1
     else
-        docker build -t "${IMAGE}" .
-        docker run -d --name="${CONTAINER}" -p 80 -p 8081 --volume=$(pwd)/${CONF_DIR}/:/home/fablab/app-server/src/dist/ "${IMAGE}"
+        build
+        run
     fi
-elif [ "$1" == "run" ]; then
-    docker run -d --name="${CONTAINER}" -p 80 -p 8081 --volume=$(pwd)/${CONF_DIR}/:/home/fablab/app-server/src/dist/ "${IMAGE}"
-elif [ "$1" == "start" ]; then
+}
+function start() {
     docker start "${CONTAINER}"
-elif [ "$1" == "stop" ]; then
+}
+function stop() {
     docker stop "${CONTAINER}"
-elif [ "$1" == "restart" ]; then
+}
+function restart() {
     docker restart "${CONTAINER}"
-elif [ "$1" == "clean" ]; then
+}
+function clean() {
     read -p "Do you really want to delete the container including ALL data? [y/N] "
     if [ "$REPLY" == "y" ]; then
         docker rm -v "${CONTAINER}"
     fi
-elif [ "$1" == "shell" ]; then
+}
+function shell() {
     docker exec -it "${CONTAINER}" "${SHELL}"
-elif [ "$1" == "logs" ]; then
+}
+function logs() {
     docker logs -f "${CONTAINER}"
-else
+}
+
+FUNCTIONS=("up" "run" "start" "stop" "restart" "build" "clean" "shell" "port" "logs")
+
+if [ -z $1 ] || [[ ! "${FUNCTIONS[@]}" =~ "$1" ]]; then
     echo "Usage: manage.sh [COMMAND]"
     echo ""
     echo "Commands:"
@@ -52,4 +68,6 @@ else
     echo "    shell    attach to running container and run a shell"
     echo "    port     query the port where the appserver is bound to"
     echo "    logs     tail the logs of dropwizard"
+else
+    $1  # run the command
 fi
